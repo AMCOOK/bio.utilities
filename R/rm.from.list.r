b96558871c8444782574222f03665628bae609f4
Cook <Adam.Cook@dfo-mpo.gc.ca>
2024-02-06 14:25:32 -0400

    up

--- R/rm.from.list.r
+++ R/rm.from.list.r
@@ -4,7 +4,7 @@
 #' @author  unknown, \email{<unknown>@@dfo-mpo.gc.ca}
 #' @export
 rm.from.list<-function(list1) {
-	a<-dim.list(list1)
+	a<-dim_list(list1)
 	if(any(a[,1]==0)) {
 	list1<-list1[-which(a[,1]==0)]
 		}
