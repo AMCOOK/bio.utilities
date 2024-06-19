460c7079cab6eb7b84c494b263c8fb92781c84c8
Cook <Adam.Cook@dfo-mpo.gc.ca>
2024-06-19 09:01:53 -0300

    na.zero update

--- R/na.zero.r
+++ R/na.zero.r
@@ -3,10 +3,18 @@
 #' @family poorly documented
 #' @author  unknown, \email{<unknown>@@dfo-mpo.gc.ca}
 #' @export
-na.zero<-function(x){
-  for(i in 1:length(x[1,])){
+na.zero<-function(x,cols=NULL){
+  if(is.null(cols)) {
+    for(i in 1:length(x[1,])){
     if(length(which(is.na(x[,i])))>0){
     x[which(is.na(x[,i])),i]<-0}
     }
+  } else {
+  for(i in 1:length(cols)){
+    if(length(which(is.na(x[,cols[i]])))>0){
+    x[which(is.na(x[,cols[i]])),cols[i]]<-0
+      }
+    }
+  }
     return(x)
   } 
